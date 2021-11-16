ecacd59179373d347f46a6a6e6a81ac97721b173
unknown <bitom@kkszki.hu>
2021-11-16 13:35:33 +0100

    mentes

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -21,3 +21,27 @@ for x in szamok:
     if x==3:
         db=db+1
 print(db)
+
+#feltételes összegzés
+osszeg=0
+for x in szamok:
+    if x%2==0:
+        osszeg=osszeg+x
+print("páros számok összege: ")
+print(osszeg)
+
+#minimum
+print("minimum")
+min=1000
+for x in szamok:
+    if x<min:
+        min=x
+print(min)
+
+#maximum
+print("maximum")
+max=1
+for x in szamok:
+    if x>max:
+        max=x
+print(max)
