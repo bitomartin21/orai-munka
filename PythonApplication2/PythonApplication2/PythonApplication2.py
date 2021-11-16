f9773985245c2a1dfa5515f01b5a1a03bed562e2
unknown <bitom@kkszki.hu>
2021-11-16 14:01:40 +0100

    mentes

--- PythonApplication2/PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -1,1 +1,30 @@
+import random
 
+szam=[]
+osszeg=0
+db=0
+for x in range(10):
+    szam.append(random.randrange(1,100))
+print(szam)
+print("átlag ",sum(szam) / 10)
+print("minimum ",min(szam))
+print("maximum ",max(szam))
+paros=False
+for x in szam:
+    if (x%2 == 0):
+        paros=True
+if paros is True:
+    print("van páros szám")
+else:
+    print("nincs páros szám")
+
+for x in szam:
+    if x>50:
+        osszeg=osszeg+x
+print("50-nél nagyobb számok összege ", osszeg)
+
+
+for x in szam:
+    if x==9:
+        db=db+1
+print("hány 9 van: " ,db)
